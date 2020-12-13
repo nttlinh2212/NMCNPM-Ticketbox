b42d3cdd1f70f42f369d7b4d7b3390467d16af87
thanhvyhcmus <thanhvy.forwork@gmail.com>
2020-12-13 16:27:08 +0700

    sign in - sign up

--- Ticketbox/routes/index.js
+++ Ticketbox/routes/index.js
@@ -8,24 +8,30 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  const films = await filmModel.all();
-  const theaters = await theaterModel.all();
-  
+    const films = await filmModel.all();
+    const theaters = await theaterModel.all();
 
-  //console.log(await showtimeModel.allSeats(30000));
-  //console.log(films);
-  res.render('index', { title: 'Home', films, theaters});
+
+    //console.log(await showtimeModel.allSeats(30000));
+    //console.log(films);
+    res.render('index', { title: 'Home', films, theaters });
+});
+router.get('/signin', function(req, res) {
+    res.render('signin');
+});
+router.get('/signup', function(req, res) {
+    res.render('signup');
 });
 //duong dan den film co dang href='/film?id=3'
 router.get('/film', async function(req, res, next) {
-  const id = req.query.id;
-  const film =  await filmModel.searchBy(id);
-  console.log("id =",id,'film',film);
-  if ( film === null){
-    console.log('Not found Film');
-    return res.redirect('/');
-  }
-    
-  res.render('film',{title: 'Film',film});
+    const id = req.query.id;
+    const film = await filmModel.searchBy(id);
+    console.log("id =", id, 'film', film);
+    if (film === null) {
+        console.log('Not found Film');
+        return res.redirect('/');
+    }
+
+    res.render('film', { title: 'Film', film });
 });
 module.exports = router;
