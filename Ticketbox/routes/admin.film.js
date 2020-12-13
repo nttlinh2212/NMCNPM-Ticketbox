8e7a14364427a36f93dfe635c1556f1c482cad38
thanhvyhcmus <thanhvy.forwork@gmail.com>
2020-12-13 21:59:02 +0700

    index page

--- Ticketbox/routes/admin.film.js
+++ Ticketbox/routes/admin.film.js
@@ -4,44 +4,44 @@ const filmModel = require('../models/film');
 
 /* GET users listing. */
 
-router.get('/', async function (req, res) {
-  const list = await filmModel.all();
-  console.log(list);
-  res.render('admin/film/index', {
-    films: list,
-    empty: list.length === 0
-  });
+router.get('/', async function(req, res) {
+    const list = await filmModel.all();
+    console.log(list);
+    res.render('admin/film/index', {
+        films: list,
+        empty: list.length === 0
+    });
 })
 
-router.get('/edit', async function (req, res) {
-  const id = req.query.id;
-  const film = await filmModel.searchBy(id);
-  if (film === null) {
-    return res.redirect('/admin/film');
-  }
+router.get('/edit', async function(req, res) {
+    const id = req.query.id;
+    const film = await filmModel.searchBy(id);
+    if (film === null) {
+        return res.redirect('/admin/film');
+    }
 
-  res.render('admin/film/edit', {
-    film
-  });
+    res.render('admin/film/edit', {
+        film
+    });
 })
 
-router.get('/add', function (req, res) {
-  res.render('admin/film/add');
+router.get('/add', function(req, res) {
+    res.render('admin/film/add');
 })
 
-router.post('/add', async function (req, res) {
-  await filmModel.add(req.body);
-  res.render('admin/film/add');
+router.post('/add', async function(req, res) {
+    await filmModel.add(req.body);
+    res.render('admin/film/add');
 })
 
-router.post('/del', async function (req, res) {
-  await filmModel.del(req.body.CatID);
-  res.redirect('/admin/film');
+router.post('/del', async function(req, res) {
+    await filmModel.del(req.body.CatID);
+    res.redirect('/admin/film');
 })
 
-router.post('/update', async function (req, res) {
-  await filmModel.update(req.body);
-  res.redirect('/admin/film');
+router.post('/update', async function(req, res) {
+    await filmModel.update(req.body);
+    res.redirect('/admin/film');
 })
 
 module.exports = router;
