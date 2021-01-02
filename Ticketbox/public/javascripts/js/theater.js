9ba33de497364e86b89ee97f34adfac8cdeeb90d
thanhvyhcmus <thanhvy.forwork@gmail.com>
2021-01-02 09:24:28 +0700

    Finish theater

---
+++ Ticketbox/public/javascripts/js/theater.js
@@ -0,0 +1,31 @@
+$('#frmDate').on('submit', function(e) {
+    e.preventDefault();
+    const date = $('#datepicker').val();
+    if (date === '') {
+        $('#showtimes').append('<h5>Please pick a day</h5>');
+        return;
+    } else {
+        $('#showtimes').text("");
+    }
+    const id = $('#theaterid').val();
+    $.getJSON(`/showtimes-groupby-film?id=${id}&date=${date}`, function(data) {
+        if (data != null) {
+            data.forEach(element => {
+                $('#showtimes').append(createShowtime(element));
+            });
+        } else {
+            $('#showtimes').append('<h5>No showtimes on this day.</h5>');
+        }
+    })
+});
+
+function createShowtime(data) {
+    var html = "";
+    html += '<div class="showtime-theater"><div class="title"><h4>' + data[0].title + '</h4></div><div class="row">';
+    data.forEach(element => {
+        html += '<div class="col-md-2 text-center"><button class="btn"' +
+            ' formaction="/customer/book-tickets/' + element.id + '">' + element.starttime.substring(0, 5) + '</button></div>';
+    });
+    html += '</div></div>';
+    return html;
+}
